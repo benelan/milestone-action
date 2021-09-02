5fe98a8ac159bd3acdbbd3d81edeb37325d63c28
Ben Elan <belan@esri.com>
2021-09-01 19:25:44 -0700

    initial changes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,88 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {WebhookPayload} from '@actions/github/lib/interfaces'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+interface Payload extends WebhookPayload {
+  pull_request?: {
+    [key: string]: unknown
+    number: number
+    html_url?: string
+    body?: string
+    milestone?: unknown
+  }
+  issue?: {
+    [key: string]: unknown
+    number: number
+    html_url?: string
+    body?: string
+    milestone?: unknown
   }
 }
 
-run()
+export const existsMilestone = (payload: Payload): boolean =>
+  !!(payload.issue?.milestone || payload.pull_request?.milestone)
+
+interface Milestone {
+  title: string
+  number: number
+  due_on: string | null
+}
+
+export const findCurrentMilestone = (milestones: {
+  data: Milestone[]
+}): Milestone => {
+  return milestones.data[0]
+}
+
+async function run(): Promise<void> {
+  const {repo, payload, issue} = github.context
+
+  if (payload.action !== 'opened') {
+    console.log('No issue or PR was opened, skipping')
+    return
+  }
+
+  // Do nothing if its not a pr or issue
+  const isIssue = !!payload.issue
+  const isPR = !!payload.pull_request
+  if (!isIssue && !isPR) {
+    console.log(
+      'The event that triggered this action was not a pull request or issue, skipping.'
+    )
+    return
+  }
+
+  if (existsMilestone(payload as Payload)) {
+    console.log('Milestone already exist, skipping.')
+    return
+  }
+
+  const myToken = core.getInput('github_token')
+  const client = github.getOctokit(myToken)
+
+  const milestones = await client.rest.issues.listMilestones({
+    ...repo,
+    state: 'open',
+    sort: 'due_on',
+    direction: 'asc'
+  })
+
+  if (milestones.data.length === 0) {
+    console.log('There are no milestones, skipping.')
+    return
+  }
+
+  const currentMilestone = findCurrentMilestone(milestones)
+
+  await client.rest.issues.update({
+    ...repo,
+    issue_number: issue.number,
+    milestone: currentMilestone.number
+  })
+  core.setOutput('milestone', currentMilestone)
+}
+
+// eslint-disable-next-line github/no-then
+run().catch(err => {
+  core.setFailed(err.message)
+})
